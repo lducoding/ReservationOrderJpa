31f88b39962438b137a776c475928af70d759449
lducoding <lducoding@gmail.com>
2022-10-20 13:40:59 +0900

    post controller 테스트 안됨

--- src/test/java/com/ldu/reservationOrder/controller/ResUserControllerTest.java
+++ src/test/java/com/ldu/reservationOrder/controller/ResUserControllerTest.java
@@ -10,11 +10,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestConstructor.AutowireMode;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.BDDMockito.given;
@@ -39,10 +41,24 @@ class ResUserControllerTest {
     @Test
     void userInfoTest() throws Exception {
         given(resUserService.userInfo(1L)).willReturn(new ResUserDto
-                (1L,"123","merlin","aaa@ka.com", UserRole.ROLE_CUSTOMER, "19940407",200));
+            (1L, "123", "merlin", "aaa@ka.com", UserRole.ROLE_CUSTOMER, "19940407", 200));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/userInfo/{resUserId}", 1L))
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(jsonPath("$.mileage").value(200));
+    }
+
+    @Test
+    void registerUserTest() throws Exception {
+        ResUserDto resUserDto = new ResUserDto(99L, "password", "마법사", "kokoa@koko.com",
+            UserRole.ROLE_SELLER, "19920415", 1000);
+
+        given(resUserService.registerUser(resUserDto)).willReturn(99L);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                .content(String.valueOf(resUserDto))
+                .contentType(MediaType.APPLICATION_JSON));
+//            .andExpect(MockMvcResultMatchers.status().isOk());
+//            .andDo(MockMvcResultHandlers.print());
     }
 }
