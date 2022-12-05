38fd0d212032760417d57456fa7098647bcd21cb
lducoding <lducoding@gmail.com>
2022-12-05 16:18:36 +0900

    페이징 처리 분석 중

---
+++ src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
@@ -0,0 +1,20 @@
+package com.ldu.reservationOrder.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RestaurantController {
+
+  @GetMapping("/restaurants")
+  public ResponseEntity<?> getRestaurantLists() {
+    HttpHeaders httpHeaders = new HttpHeaders();
+    return new ResponseEntity<>(, httpHeaders, HttpStatus.OK);
+  }
+}
