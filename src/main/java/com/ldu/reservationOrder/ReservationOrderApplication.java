9bf4564a851f7c32f4549bc4687f2a712d14aa2f
lducoding <lducoding@gmail.com>
2022-12-07 23:18:08 +0900

    생성일과 업데이트일 공통적용 엔티티 생성

--- src/main/java/com/ldu/reservationOrder/ReservationOrderApplication.java
+++ src/main/java/com/ldu/reservationOrder/ReservationOrderApplication.java
@@ -5,7 +5,9 @@ import javax.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ReservationOrderApplication {
 
