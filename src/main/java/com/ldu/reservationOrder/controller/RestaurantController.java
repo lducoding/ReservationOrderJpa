8d4127239f6c7bdcc22ea126c79e89a89eead6ba
lducoding <lducoding@gmail.com>
2022-12-10 19:45:02 +0900

    good

--- src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
+++ src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
@@ -16,6 +16,7 @@ public class RestaurantController {
   @GetMapping("/restaurants")
   public ResponseEntity<?> getRestaurantLists() {
     HttpHeaders httpHeaders = new HttpHeaders();
+    // elk 설치해서 한글 자모 분리해서 검색하는 거 완료함!!!! 개쩐다 시바 ㅋㅋㅋㅋㅋㅋ
     return new ResponseEntity<>("", httpHeaders, HttpStatus.OK);
   }
 }
