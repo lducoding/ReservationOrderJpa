03e7142d5aadf3a9748fd61792b22baa8b3e5492
lducoding <lducoding@gmail.com>
2022-12-11 22:19:47 +0900

    메인화면 레스토랑 리스트 불러오기 만드는 중

--- src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
+++ src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
@@ -1,22 +1,23 @@
 package com.ldu.reservationOrder.controller;
 
-import com.ldu.reservationOrder.dto.MenuDto;
+import com.ldu.reservationOrder.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class RestaurantController {
 
+  private final RestaurantService restaurantService;
+
   @GetMapping("/restaurants")
   public ResponseEntity<?> getRestaurantLists() {
     HttpHeaders httpHeaders = new HttpHeaders();
-    // elk 설치해서 한글 자모 분리해서 검색하는 거 완료함!!!! 개쩐다 시바 ㅋㅋㅋㅋㅋㅋ
+    restaurantService.
     return new ResponseEntity<>("", httpHeaders, HttpStatus.OK);
   }
 }
---
+++ src/main/java/com/ldu/reservationOrder/service/RestaurantService.java
@@ -0,0 +1,13 @@
+package com.ldu.reservationOrder.service;
+
+import com.ldu.reservationOrder.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+}
