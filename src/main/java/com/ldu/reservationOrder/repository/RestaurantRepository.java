71e4d4342a9976c9f3c9031a6660d8b443aa57b1
lducoding <lducoding@gmail.com>
2022-12-16 14:14:10 +0900

    pageable 처리 중

--- src/main/java/com/ldu/reservationOrder/repository/RestaurantRepository.java
+++ src/main/java/com/ldu/reservationOrder/repository/RestaurantRepository.java
@@ -1,8 +1,12 @@
 package com.ldu.reservationOrder.repository;
 
 import com.ldu.reservationOrder.entity.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+  Page<Restaurant> findAllRestaurantPaging(Pageable pageable);
 
 }
--- src/main/java/com/ldu/reservationOrder/service/RestaurantService.java
+++ src/main/java/com/ldu/reservationOrder/service/RestaurantService.java
@@ -4,6 +4,9 @@ import com.ldu.reservationOrder.dto.RestaurantDto;
 import com.ldu.reservationOrder.entity.Restaurant;
 import com.ldu.reservationOrder.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,21 +16,32 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RestaurantService {
 
-    private final RestaurantRepository restaurantRepository;
+  private final RestaurantRepository restaurantRepository;
 
-    public List<RestaurantDto> getRestaurantLists() {
-        return restaurantRepository.findAll().stream().map(
-                res -> {
-                    return RestaurantDto.builder()
-                            .restaurantId(res.getRestaurantId())
-                            .resUserId(res.getResUser().getResUserId())
-                            .goalId(res.getGoal().getGoalId())
-                            .location(res.getLocation())
-                            .restaurantName(res.getRestaurantName())
-                            .category(res.getCategory())
-                            .standardTime(res.getStandardTime())
-                            .build();
-                }
+  public Page<RestaurantDto> getRestaurantLists(Pageable pageable) {
+
+    PageRequest pageRequest = PageRequest.of(page, size);
+    int start = (int) pageRequest.getOffset();
+    int end = Math.min((start + pageRequest.getPageSize()), userList.size());
+    Page<User> userPage = new PageImpl<>(userList.subList(start, end), pageRequest, userList.size());
+
+    List<RestaurantDto> collect = restaurantRepository.findAllRestaurantPaging(pageable).stream()
+        .map(
+            res -> {
+              return RestaurantDto.builder()
+                  .restaurantId(res.getRestaurantId())
+                  .resUserId(res.getResUser().getResUserId())
+                  .goalId(res.getGoal().getGoalId())
+                  .location(res.getLocation())
+                  .restaurantName(res.getRestaurantName())
+                  .category(res.getCategory())
+                  .standardTime(res.getStandardTime())
+                  .build();
+            }
         ).collect(Collectors.toList());
-    }
+
+    new PageImpl<>(collect.)
+
+    return
+  }
 }
