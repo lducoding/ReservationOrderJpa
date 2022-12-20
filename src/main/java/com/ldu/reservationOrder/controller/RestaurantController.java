bc928a05637d8572b4f79c7d3c6dae1d9d527242
lducoding <lducoding@gmail.com>
2022-12-20 22:59:48 +0900

    레스토랑 검색 분석

--- src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
+++ src/main/java/com/ldu/reservationOrder/controller/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.ldu.reservationOrder.controller;
 
 import com.ldu.reservationOrder.dto.RestaurantDto;
+import com.ldu.reservationOrder.dto.RestaurantSerchDto;
 import com.ldu.reservationOrder.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,12 +19,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RestaurantController {
 
-  private final RestaurantService restaurantService;
+    private final RestaurantService restaurantService;
 
-  @GetMapping("/restaurants")
-  public ResponseEntity<Page<RestaurantDto>> getRestaurantLists(Pageable pageable) {
-    HttpHeaders httpHeaders = new HttpHeaders();
-    Page<RestaurantDto> restaurantLists = restaurantService.getRestaurantLists(pageable);
-    return new ResponseEntity<Page<RestaurantDto>>(restaurantLists, httpHeaders, HttpStatus.OK);
-  }
+    @GetMapping("/restaurants")
+    public ResponseEntity<Page<RestaurantDto>> getRestaurantLists(Pageable pageable) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        Page<RestaurantDto> restaurantLists = restaurantService.getRestaurantLists(pageable);
+        return new ResponseEntity<Page<RestaurantDto>>(restaurantLists, httpHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping("/searchRestaurants")
+    public ResponseEntity<?> getSearchRestaurantLists(@RequestParam(defaultValue = "1") Integer pageNum,
+                                                     @RequestParam(defaultValue = "8") Integer pageSize,
+                                                     RestaurantSerchDto restaurantSerchDto) {
+      // dto의 값이 있는 걸로만 검색
+        return new ResponseEntity<>(restaurantDtoPageInfo, HttpStatus.OK);
+    }
 }
