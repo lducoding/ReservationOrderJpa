90415248273801fbcbb15237295bb232209f725d
lducoding <lducoding@gmail.com>
2022-12-29 18:17:52 +0900

    goaldto 추가

---
+++ src/main/java/com/ldu/reservationOrder/dto/GoalDto.java
@@ -0,0 +1,16 @@
+package com.ldu.reservationOrder.dto;
+
+import lombok.Data;
+
+@Data
+public class GoalDto {
+
+    private Long goalId;
+    private Long restaurantId;
+    private String goalType;
+    private int goalMoney;
+    private int recentMoney;
+    private String success;
+    private String registerDate;
+
+}
